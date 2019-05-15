594a0da88d039ac7123dd5a057722af4c1f0d2df
Johnson <zxmagichacker@gmail.com>
2019-05-15 12:48:04 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from "lodash";
+import "./style.css";
+import printMe from "./print.js";
+const component = () => {
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
+  element.innerHTML = _.join(["Hello ", "Webpack"], " ");
+  element.classList.add("hello");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+  element.appendChild(btn);
+  return element;
+};
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpaclPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpaclPlugin({
+      title: "Output",
+      template: "./src/index.html"
+    })
+  ]
+};
