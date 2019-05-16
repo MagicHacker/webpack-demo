a11d28269cd522b31980c3a358dc0ca34f7a995c
Johnson <zxmagichacker@gmail.com>
2019-05-16 16:30:21 +0800

    feat: 增加babel的配置文件将babel的配置从webpack中提出

--- webpack.base.js
+++ webpack.base.js
@@ -35,13 +35,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-runtime"]
-          }
-        }
+        use: "babel-loader"
       }
     ]
   },
